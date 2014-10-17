d120cbdd67847e590d698a35323c6343fe442cde
Teo Panda <antonio@publiaciertos.net>
2014-10-16 22:58:12 -0400

    Add rol al usuario al hacer login

--- class/class.mapper.php
+++ class/class.mapper.php
@@ -3,20 +3,24 @@ include_once 'class.database.php';
 class Mapper {	
 	
 	public static function login($user, $pass) {
-		$sql_query = "SELECT * FROM usuario WHERE username='$user' and password='$pass'";
+		$sql_query = "SELECT * FROM usuario WHERE username='$user' AND password='$pass'";
 		$result = self::query($sql_query);
 		
 		if(mysqli_num_rows($result) == 1){			
 			$_SESSION['username']=$user;
-		}		
+			$sql_query = "SELECT nombre_rol FROM rol, usuario_rol WHERE usuario_username ='$user' AND rol_id_rol=id_rol";			
+			echo $sql_query;			
+			$result = self::query($sql_query);
+			$row = mysqli_fetch_assoc($result);
+			$_SESSION['rol']=$row['nombre_rol'];
+		}
 		mysqli_free_result($result);
 	}
 	
 	private static function query($sql_query){
 		$db = Database::getInstance();
 		$mysqli = $db->getConnection();
-		$result = $mysqli->query($sql_query) or die('La consulta fallo: ' . mysqli_connect_error());
-		mysqli_close($mysqli);		
+		$result = $mysqli->query($sql_query) or die('La consulta fallo: ' . mysqli_error($mysqli));		
 		return $result; 
 	}
 }
